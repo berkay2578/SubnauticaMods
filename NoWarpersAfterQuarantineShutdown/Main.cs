8f8fa7bdbcc170dc2721cdf500fb77d7b44c02f3
Berkay Yiğit <berkaytgy@gmail.com>
2018-05-03 18:50:27 +0300

    v1.1.0.0: Fixed bugs with Harmony

--- NoWarpersAfterQuarantineShutdown/Main.cs
+++ NoWarpersAfterQuarantineShutdown/Main.cs
@@ -1,38 +1,53 @@
-﻿using Harmony;
+﻿using System;
+using System.Reflection;
+using Harmony;
 
 namespace NoWarpersAfterQuarantineShutdown {
    public static class Patcher {
+      public static void log(string logMessage, params string[] arg) {
+         if (arg.Length > 0)
+            logMessage = String.Format(logMessage, arg);
+         Console.WriteLine("[NoWarpersAfterQuarantineShutdown] {0}", logMessage);
+      }
+
       public class ManageWarperSpawns {
+         [HarmonyPrefix]
          [HarmonyPriority(Priority.First)]
          public static bool GenericSkipExecutionPatch() {
             if (StoryGoalCustomEventHandler.main.gunDisabled)
                return false;
             return true;
          }
-
-         public class ManageStoryEvents {
-            public static void PrecursorGunDisabledNotification() {
-               ErrorMessage.AddMessage("Self-Warping Quarantine Enforcer Units are now disabled.");
-            }
-            public static void AwakeGunDisabledCheck(StoryGoalCustomEventHandler __instance) {
-               if (__instance.gunDeactivate.key != null)
-                  if (Story.StoryGoalManager.main.IsGoalComplete(__instance.gunDeactivate.key))
-                     ManageStoryEvents.PrecursorGunDisabledNotification();
-            }
+      }
+      public class ManageStoryEvents {
+         [HarmonyPostfix]
+         public static void PrecursorGunDisabledNotification() {
+            ErrorMessage.AddMessage("Self-Warping Quarantine Enforcer Units are now disabled.");
+         }
+         [HarmonyPostfix]
+         public static void AwakeGunDisabledCheck(StoryGoalCustomEventHandler __instance) {
+            if (__instance.gunDeactivate != null && !String.IsNullOrEmpty(__instance.gunDeactivate.key))
+               if (Story.StoryGoalManager.main.IsGoalComplete(__instance.gunDeactivate.key))
+                  ManageStoryEvents.PrecursorGunDisabledNotification();
          }
       }
 
       public static void ApplyPatches() {
+         log("Loading... {0}", Assembly.GetExecutingAssembly().GetName().Version.ToString());
+
          HarmonyInstance harmony = HarmonyInstance.Create("mod.berkay2578.disablewarpers");
          if (harmony != null)
          {
+            log("HarmonyInstance created.");
+
             // StoryGoalCustomEventHandler.Awake (Post)
             {
                harmony.Patch(
-                   typeof(StoryGoalCustomEventHandler).GetMethod("Awake"),
+                   typeof(StoryGoalCustomEventHandler).GetMethod("Awake", BindingFlags.NonPublic | BindingFlags.Instance),
                    null,
-                   new HarmonyMethod(typeof(ManageWarperSpawns.ManageStoryEvents).GetMethod("AwakeGunDisabledCheck"))
+                   new HarmonyMethod(typeof(ManageStoryEvents).GetMethod("AwakeGunDisabledCheck"))
                );
+               log("StoryGoalCustomEventHandler.Awake (Post)");
             }
 
             // StoryGoalCustomEventHandler.DisableGun (Post)
@@ -40,31 +55,35 @@ namespace NoWarpersAfterQuarantineShutdown {
                harmony.Patch(
                    typeof(StoryGoalCustomEventHandler).GetMethod("DisableGun"),
                    null,
-                   new HarmonyMethod(typeof(ManageWarperSpawns.ManageStoryEvents).GetMethod("PrecursorGunDisabledNotification"))
+                   new HarmonyMethod(typeof(ManageStoryEvents).GetMethod("PrecursorGunDisabledNotification"))
                );
+               log("StoryGoalCustomEventHandler.DisableGun (Post)");
             }
 
             // WarperSpawner.OnEnable (Pre)
             {
                harmony.Patch(
-                   typeof(WarperSpawner).GetMethod("OnEnable"),
+                   typeof(WarperSpawner).GetMethod("OnEnable", BindingFlags.NonPublic | BindingFlags.Instance),
                    new HarmonyMethod(typeof(ManageWarperSpawns).GetMethod("GenericSkipExecutionPatch")),
                    null
                );
+               log("WarperSpawner.OnEnable (Pre)");
             }
 
             // WarperSpawner.Update (Pre)
             {
                harmony.Patch(
-                  typeof(WarperSpawner).GetMethod("Update"),
+                  typeof(WarperSpawner).GetMethod("Update", BindingFlags.NonPublic | BindingFlags.Instance),
                   new HarmonyMethod(typeof(ManageWarperSpawns).GetMethod("GenericSkipExecutionPatch")),
                   null
                );
+               log("WarperSpawner.Update (Pre)");
             }
+            log("Patched successfully.");
          }
          else
          {
-            ErrorMessage.AddError("[NoWarpersAfterQuarantineShutdown] Harmony couldn't be initialized.");
+            log("HarmonyInstance() returned null.");
          }
       }
    }
