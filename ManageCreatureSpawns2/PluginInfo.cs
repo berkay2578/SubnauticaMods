8965e937a0c8eb6edfd8176a009666dccb294147
GRex-work <91720800+GRex-work@users.noreply.github.com>
2022-12-29 01:16:33 -0500

    Update plugin info
    Mod GUID should now be unique unless somebody is trying to impersonate
    me. Version is now set to 2.0.0 to make things easier for updating the
    existing mod page.

---
+++ ManageCreatureSpawns2/PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace ManageCreatureSpawns2
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "com.arnoldrex.mods.subnautica.managecreaturespawns2";
+        public const string PLUGIN_NAME = "ManageCreatureSpawns2";
+        public const string PLUGIN_VERSION = "2.0.0";
+    }
+}
